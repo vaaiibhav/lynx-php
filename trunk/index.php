c46f55d33aef79b2b2bf3a5ef37a0b6a55c519dd
spechal <spechal@f74850e0-1aa2-11df-99a4-2df76caecac5>
2010-02-16 06:44:11 +0000

    Working on Template stuff

--- trunk/index.php
+++ trunk/index.php
@@ -2,23 +2,19 @@
   ini_set('error_reporting', E_ALL | E_STRICT);
   ini_set('display_errors', 'On');
   
-  set_include_path(getcwd().DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'Lynx'.PATH_SEPARATOR.get_include_path());
+  set_include_path(getcwd().DIRECTORY_SEPARATOR.'lib'.PATH_SEPARATOR.get_include_path());
   
-  require_once('Error.php');
+  require_once('Lynx/Error.php');
   #set_error_handler(array(new Lynx_Error, 'error'), E_ALL | E_STRICT);
 
-  require_once('Registry.php');
-  require_once('Template.php');
-  require_once('Controller/Front.php');
+  require_once('Lynx/Session.php');
+  require_once('Lynx/Registry.php');
+  require_once('Lynx/Controller/Front.php');
+  
+  $Session = new Lynx_Session();
   
   $Registry = Lynx_Registry::getInstance();
   
   $controller = Lynx_Controller_Front::getInstance();
   
-  $Template = Lynx_Template::getInstance();
-  
-  $Registry->set('template', $Template);
-  
   $controller->run($Registry);
-  
-  echo '<pre>'; print_r($_REQUEST);
