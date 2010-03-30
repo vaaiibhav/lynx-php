7b9c2c1fe2d33ef9981951673a5b652d735c2f92
spechal <spechal@f74850e0-1aa2-11df-99a4-2df76caecac5>
2010-03-30 23:10:20 +0000

    Adding GUID Validator

---
+++ trunk/lib/Lynx/Validator/Validator_GUID.php
@@ -0,0 +1,22 @@
+<?php
+  
+  /**
+   * @category Lynx
+   * @package Lynx_Validator_GUID
+   * @author Travis Crowder
+   * @version $Id$
+   */
+
+  require_once('Lynx/Validator/Validator_Abstract.php');
+
+  class Lynx_Validator_GUID extends Lynx_Validator_Abstract {
+  	
+  	public function __construct($data = NULL){
+  		$this->_data = $data;
+  	}
+  	
+  	public function isValid(){
+  		return preg_match('#^[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}$#', $this->_data);
+  	}
+  	
+  }
