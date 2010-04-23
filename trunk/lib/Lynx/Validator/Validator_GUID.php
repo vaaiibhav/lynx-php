e5e58a4a8f8fcd3311d48f5501fcceea7d8a768b
spechal <spechal@f74850e0-1aa2-11df-99a4-2df76caecac5>
2010-04-23 00:06:45 +0000

    Adding Validator_GUID and Validator_Email

--- trunk/lib/Lynx/Validator/Validator_GUID.php
+++ trunk/lib/Lynx/Validator/Validator_GUID.php
@@ -10,13 +10,13 @@
   require_once('Lynx/Validator/Validator_Abstract.php');
 
   class Lynx_Validator_GUID extends Lynx_Validator_Abstract {
-  	
-  	public function __construct($data = NULL){
-  		$this->_data = $data;
-  	}
-  	
-  	public function isValid(){
-  		return preg_match('#^[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}$#', $this->_data);
-  	}
-  	
+    
+    public function __construct($data = NULL){
+      $this->_data = $data;
+    }
+    
+    public function isValid(){
+      return preg_match('#^[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}$#', $this->_data);
+    }
+    
   }
