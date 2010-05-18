fbce2abbef44deaea8768657655455c5e25813e9
spechal <spechal@f74850e0-1aa2-11df-99a4-2df76caecac5>
2010-05-18 21:10:30 +0000

    Adding some comments to js.js

--- trunk/templates/default/scripts/js.js
+++ trunk/templates/default/scripts/js.js
@@ -1,5 +1,8 @@
+/** global request variable -- holds the xmlHttpRequest object **/
 var request = null;
 
+/** function to create an XMLHttpRequest 
+ * object and assign it to the request variable **/
 function createRequest(){
   try {
     request = new XMLHttpRequest();
@@ -23,6 +26,9 @@ function createRequest(){
 
 }
 
+/** function to easily get elements by id or name 
+ * passing a name will return either the top level named 
+ * item or an array of items with the name, whichever is found **/
 function $(id){
 	var e = document.getElementById(id);
 	if(e != undefined){
@@ -41,6 +47,16 @@ function $(id){
 		
 }
 
+/** function to easily getElementsByName **/
+function $$(name){
+	e = document.getElementsByName(name);
+	if(e != undefined)
+		return e
+	return false;
+}
+
+/** function to display a JS confirm box displaying the value 
+ * of the msg variable and redirecting to target on true **/
 function checkConfirm(msg, target){
   if(confirm(msg))
     window.location.href = target;
@@ -48,6 +64,7 @@ function checkConfirm(msg, target){
     return false;
 }
 
+/** function to add a bookmark to the browser **/
 function addBookmark(title, url){
   if(window.sidebar)
     window.sidebar.addPanel(title, url, "");
